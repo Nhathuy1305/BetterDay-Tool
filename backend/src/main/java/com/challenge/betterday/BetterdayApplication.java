6568428928807f1cd2276ef29c31e5cf0bb0df20
Nhat Huy Dang <89309283+Nhathuy1305@users.noreply.github.com>
2023-08-14 04:21:53 +0000

    Add Spring Back-end

---
+++ backend/src/main/java/com/challenge/betterday/BetterdayApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.betterday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetterdayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetterdayApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/challenge/betterday/BetterdayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.betterday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetterdayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
