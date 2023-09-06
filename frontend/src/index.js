43a1e551997ea97f281c2b3462afa9f9f8826294
BUI THI CAM <V.BUITHICAM@bycn.bouygues-construction.com>
2023-09-06 15:56:39 +0700

    add css

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,7 @@ import "bootstrap/dist/css/bootstrap.css";
 import "./index.css";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));  
 root.render(
   <React.StrictMode>
     <App />
