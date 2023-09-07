a249473f639a37c15d6d92b410cb9499f33fe7da
Nhat Huy Dang <89309283+Nhathuy1305@users.noreply.github.com>
2023-09-07 15:26:19 +0700

    Update pom.xml

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-:bangbang: PLEASE READ THIS CAREFULLY BEFORE CODING :bangbang:
 
-Branches checkout: MASTER :arrow_right: DEV :arrow_right: #feature-branches
--- backend/src/main/java/com/challenge/betterday/repository/ItemRepository.java
+++ backend/src/main/java/com/challenge/betterday/repository/ItemRepository.java
@@ -2,6 +2,8 @@ package com.challenge.betterday.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.challenge.betterday.entity.Item;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
 }
