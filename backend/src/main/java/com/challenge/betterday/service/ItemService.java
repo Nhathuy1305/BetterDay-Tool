54c8fd4f698c039b25caa8aae5f9241fb8dfeac4
Nhat Huy Dang <89309283+Nhathuy1305@users.noreply.github.com>
2023-09-07 15:48:46 +0700

    Update Docker

--- backend/src/main/java/com/challenge/betterday/service/ItemService.java
+++ backend/src/main/java/com/challenge/betterday/service/ItemService.java
@@ -1,56 +1,56 @@
-package com.challenge.betterday.service;
+// package com.challenge.betterday.service;
 
-import com.challenge.betterday.entity.Item;
-import io.weaviate.client.WeaviateClient;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+// import com.challenge.betterday.entity.Item;
+// import io.weaviate.client.WeaviateClient;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.stereotype.Service;
 
-@Service
-public class ItemService {
-    private final WeaviateClient weaviateClient;
+// @Service
+// public class ItemService {
+//     private final WeaviateClient weaviateClient;
 
-    @Autowired
-    public ItemService(WeaviateClient weaviateClient) {
-        this.weaviateClient = weaviateClient;
-    }
+//     @Autowired
+//     public ItemService(WeaviateClient weaviateClient) {
+//         this.weaviateClient = weaviateClient;
+//     }
 
-    public Item getItemById(String itemId) {
-        try {
-            // Use Weaviate client to retrieve an item by its ID
-            return weaviateClient.getItemById(itemId);
-        } catch (WeaviateClientException e) {
-            // Handle the exception, e.g., log it or throw a custom exception
-            throw new ItemNotFoundException("Item not found", e);
-        }
-    }
+//     public Item getItemById(String itemId) {
+//         try {
+//             // Use Weaviate client to retrieve an item by its ID
+//             return weaviateClient.getItemById(itemId);
+//         } catch (WeaviateClientException e) {
+//             // Handle the exception, e.g., log it or throw a custom exception
+//             throw new ItemNotFoundException("Item not found", e);
+//         }
+//     }
 
-    public Item createItem(Item newItem) {
-        try {
-            // Use Weaviate client to create a new item
-            return weaviateClient.createItem(newItem);
-        } catch (WeaviateClientException e) {
-            // Handle the exception, e.g., log it or throw a custom exception
-            throw new ItemCreationException("Failed to create item", e);
-        }
-    }
+//     public Item createItem(Item newItem) {
+//         try {
+//             // Use Weaviate client to create a new item
+//             return weaviateClient.createItem(newItem);
+//         } catch (WeaviateClientException e) {
+//             // Handle the exception, e.g., log it or throw a custom exception
+//             throw new ItemCreationException("Failed to create item", e);
+//         }
+//     }
 
-    public Item updateItem(String itemId, Item updatedItem) {
-        try {
-            // Use Weaviate client to update an existing item
-            return weaviateClient.updateItem(itemId, updatedItem);
-        } catch (WeaviateClientException e) {
-            // Handle the exception, e.g., log it or throw a custom exception
-            throw new ItemUpdateException("Failed to update item", e);
-        }
-    }
+//     public Item updateItem(String itemId, Item updatedItem) {
+//         try {
+//             // Use Weaviate client to update an existing item
+//             return weaviateClient.updateItem(itemId, updatedItem);
+//         } catch (WeaviateClientException e) {
+//             // Handle the exception, e.g., log it or throw a custom exception
+//             throw new ItemUpdateException("Failed to update item", e);
+//         }
+//     }
 
-    public void deleteItem(String itemId) {
-        try {
-            // Use Weaviate client to delete an item
-            weaviateClient.deleteItem(itemId);
-        } catch (WeaviateClientException e) {
-            // Handle the exception, e.g., log it or throw a custom exception
-            throw new ItemDeletionException("Failed to delete item", e);
-        }
-    }
-}
+//     public void deleteItem(String itemId) {
+//         try {
+//             // Use Weaviate client to delete an item
+//             weaviateClient.deleteItem(itemId);
+//         } catch (WeaviateClientException e) {
+//             // Handle the exception, e.g., log it or throw a custom exception
+//             throw new ItemDeletionException("Failed to delete item", e);
+//         }
+//     }
+// }
